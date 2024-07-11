1ef923e44e37e1ca42f6c43ff4c9c1e04d8ec534
Mikh12-byte <opus.life24@gmail.com>
2024-07-11 16:25:11 +0700

    Add files via upload

---
+++ Mikh.js
@@ -0,0 +1,10 @@
+function clicks() {
+    $('#div1').hide();
+    $('#div2').show();
+    $('#div2').addClass("fadeinv2");
+        
+    var typed = new Typed('.katakata', {
+        strings: ["I Wanna say if", "", "Your gaze is simple, but it can convey the world", "Love You"],
+        typeSpeed: 150
+    });
+}
